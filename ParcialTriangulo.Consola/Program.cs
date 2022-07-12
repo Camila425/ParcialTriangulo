a836b780bb7561de05ea0c8c27e2aecd574d38f8
camila <camila@LAPTOP-N0GHACTF>
2022-07-11 23:50:51 -0300

    Agregar archivos de proyecto.

---
+++ ParcialTriangulo.Consola/Program.cs
@@ -0,0 +1,29 @@
+﻿using ParcialTriangulo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialTriangulo.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            triangulo Triangulo = new triangulo();
+
+            Console.Write("ingrese el ladoA:");
+            int ladoA = int.Parse(Console.ReadLine());
+            Console.Write("ingrese el ladoB:");
+            int ladoB = int.Parse(Console.ReadLine());
+            Console.Write("ingrese el ladoC:");
+            int Ladoc = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"triangulo es:{Triangulo.GetTriangulo()}");
+            Console.WriteLine($"perimetro es:{triangulo.GetPerimetro()}");
+         
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ParcialTriangulo.Entidades/triangulo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialTriangulo.Entidades
+{
+    public class triangulo
+    {
+        private static int ladoA;
+        private static int ladoB;
+        private static int Ladoc;
+
+        public triangulo(int ladoa, int ladob, int ladoc)
+        {
+            ladoA = ladoa;
+            ladoB = ladob;
+            Ladoc = ladoc;
+        }
+
+        public triangulo()
+        {
+            ladoA = 1;
+            ladoB = 1;
+            Ladoc = 1;
+        }
+
+
+        public static bool Validar()
+        {
+            return ladoA + ladoB > Ladoc && ladoA + Ladoc > ladoB && ladoB + Ladoc > ladoA;
+        }
+
+        public string GetTriangulo()
+        {
+            string Resultado = "";
+
+            if (Math.Pow(ladoA,2)==Math.Pow(ladoB, 2) + Math.Pow(Ladoc, 2))
+            {
+                Resultado=("triangulo rectangulo");
+            }
+            if (Math.Pow(ladoA,2) > Math.Pow(ladoB, 2) + Math.Pow(Ladoc, 2))
+            {
+                Resultado=("triangulo obtusangulo");
+            }
+
+            if (Math.Pow(ladoA, 2)< Math.Pow(ladoB, 2) + Math.Pow(Ladoc, 2))
+            {
+                Resultado=("triangulo acutángulo");
+            }
+            return Resultado;
+        }
+
+        public static double GetPerimetro()
+        {
+
+            return ladoA + ladoB + Ladoc;
+        }
+
+        public static double SemiPerimetro()
+        {
+            return (ladoA + ladoB + Ladoc) / 2;
+        }
+
+
+
+
+    }
+}
